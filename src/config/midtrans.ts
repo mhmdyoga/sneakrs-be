015bb6940e0eb071e894a29b5947c6a07d42003f
Yoga <yogacahayq@gmail.com>
2026-01-21 13:11:37 +0700

    put: isProduction_true_at_Config_midtrans

--- src/config/midtrans.ts
+++ src/config/midtrans.ts
@@ -3,6 +3,6 @@ import MidtransClient from "midtrans-client";
 export const snap = new MidtransClient.Snap({
     serverKey: process.env.SERVER_KEY_MIDTRANS as string,
     clientKey: process.env.CLIENT_KEY_MIDTRANS as string,
-    isProduction: false
+    isProduction: true
 })
 
