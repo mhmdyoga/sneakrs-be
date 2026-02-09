e05f74e2fe39d811de7ce7ee718c8f412d478d46
Yoga <yogacahayq@gmail.com>
2026-02-10 00:40:20 +0700

    put: remove_middleware_at_transaction_endpoint

--- src/routes/tx.routes.ts
+++ src/routes/tx.routes.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get("/transactions", verifyToken, getTx);
 router.get("/transaction/:id", verifyToken, getTxById);
-router.post("/transaction", verifyToken, createTx); 
+router.post("/transaction", createTx); 
 router.post("/notification", express.raw({ type: 'application/json' }), notification);
 
 export default router;
