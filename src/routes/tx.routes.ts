138cae2b51821a623581567bc234f7fab4f7366b
Yoga <yogacahayq@gmail.com>
2026-02-22 02:25:24 +0700

    put: middleware_at_route

--- src/routes/tx.routes.ts
+++ src/routes/tx.routes.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import { createTx, getTx, getTxById, notification } from "../controllers/tx.controller.js";
-import { verifyToken } from "../middleware/token-verify.js";
 
 const router = express.Router();
 
-router.get("/transactions", verifyToken, getTx);
-router.get("/transaction/:id", verifyToken, getTxById);
+router.get("/transactions",  getTx);
+router.get("/transaction/:id",  getTxById);
 router.post("/snap", createTx); 
 router.post("/notification", express.raw({ type: 'application/json' }), notification);
 
